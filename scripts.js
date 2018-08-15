14b9710d6cb8b7118fd5291d7df04351e66443a0
Torben J <torbenjepsen@outlook.com>
2018-08-15 17:33:37 -0500

    added delete

--- scripts.js
+++ scripts.js
@@ -6,20 +6,41 @@ let clickBlue= 0;
 function onReady() {
     
     $('#redButton').on('click', function() {
-        $('#newBox').append('<div class="box red"></div>');
-        $('#redCount').text('Red Count:' + ++clickRed);
+        $('#newBox').append('<div class="box red"></div>').on('click', function() {
+            $(this).closest('div').empty();
+            $('#redCount').text(--clickRed);
+            console.log('delete clicked');
+        });
+        $('#redCount').text(++clickRed);
     });
     $('#yellowButton').on('click', function() {
-        $('#newBox').append('<div class="box yellow"></div>');
-        $('#yellowCount').text('Yellow Count:' + ++clickYellow);
+        $('#newBox').append('<div class="box yellow"></div>').on('click', function() {
+            $(this).closest('div').empty();
+            $('#yellowCount').text(--clickYellow);
+        });
+        $('#yellowCount').text(++clickYellow);
     })
     $('#greenButton').on('click', function() {
-        $('#newBox').append('<div class="box green"></div>');
-        $('#greenCount').text('Green Count:' + ++clickGreen);
+        $('#newBox').append('<div class="box green"></div>').on('click', function() {
+            $(this).closest('div').empty();
+            $('#greenCount').text(--clickGreen);
+        });
+        $('#greenCount').text(++clickGreen);
     })
     $('#blueButton').on('click', function() {
-        $('#newBox').append('<div class="box blue"></div>');
-        $('#blueCount').text('Blue Count:' + ++clickBlue);
+        $('#newBox').append('<div class="box blue"></div>').on('click', function() {
+            $(this).closest('div').empty();
+            $('#blueCount').text(--clickBlue);
+        });
+        $('#blueCount').text(++clickBlue);
     });
 
+    // $('#newBox').on('click', deleteBlock);
+
+
 }
+
+// function deleteBlock() {
+//     $(this).closest('div').empty();
+//     console.log('delete clicked');
+// }
